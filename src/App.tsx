dd14fe9b5f4f4307d4e8cbf9c5ca53ce065e3f6b
kaichi <hellokitty321ds@gmail.com>
2021-03-30 00:01:39 +0800

    add preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { FC, useState } from "react";
+import ReactMarkdown from "react-markdown";
 import "./App.css";
 import ReactCodeMirror from "./CodeMirror";
+import gfm from "remark-gfm";
 
 const storageKey = "online-markdown-editor";
 
-function App() {
+const App: FC = () => {
+  const [value, setValue] = useState<string>(() => {
+    return localStorage.getItem(storageKey) ?? "";
+  });
+
   return (
-    <ReactCodeMirror
-      value={localStorage.getItem(storageKey) ?? ""}
-      lineNumbers
-      lineWrapping
-      theme="custom"
-      keyMap="sublime"
-      mode="markdown"
-      onChange={(value) => localStorage.setItem(storageKey, value)}
-    />
+    <div className="app">
+      <ReactCodeMirror
+        value={value}
+        lineNumbers
+        lineWrapping
+        theme="custom"
+        keyMap="sublime"
+        mode="markdown"
+        onChange={(value) => {
+          setValue(value);
+          localStorage.setItem(storageKey, value);
+        }}
+      />
+      <div className="react-markdown">
+        <ReactMarkdown plugins={[gfm]} children={value} />
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
